31c08cd07dd1c8062a4fcbbd53b3bc29a67de739
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-08-22 16:40:34 -0400

    finishing the nav bar routes

--- client/src/components/Nav_bar.js
+++ client/src/components/Nav_bar.js
@@ -3,9 +3,22 @@ import styled from "styled-components";
 const Container = styled.div`
 `
 
+const Link = styled(link)`
+`
+
+
 function NavBar(){
     return(
         <Container>
+            <Link to='/home'>
+                Home
+            </Link>
+            <Link to="/${Username interpotalion}">
+                User Information
+            </Link>
+            <Link to="games">
+                Games List 
+            </Link>
 
         </Container>
     )
