5da60af7f1323a599aa8a639a6fa8204741ea64f
Trey <treyfields351@gmail.com>
2022-08-24 16:48:51 -0400

    Modified player controller and model player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,29 @@
 class PlayersController < ApplicationController
-end
+    before_action :authorize, only: [:show]
+
+    def create
+      user = User.create(player_params)
+      if player.valid?
+        session[:player_id] = player.id
+        render json: player, status: :created
+      else
+        render json: { error: player.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def show
+      player = Player.find_by(id: session[:player_id])
+      render json: player
+    end
+  
+    private
+  
+    def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+  
+    def user_params
+      params.permit(:username, :password, :password_confirmation)
+    end
+  
+  end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+    has_secure_password
+    
     has_many :cards
     belongs_to :game_table
 
