a94d929a72fb67dcb5625806b0412579f76fc7e8
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-08-26 17:32:25 -0400

    New adds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,28 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorize, only: [:index, :create]
+    # skip_before_action :authorize, only: [:index, :create]
     # # for session and cookie management
-    def index
-        session[:player_id] ||= "hello"
-        render json: {session: session}
+    # def index
+    #     session[:player_id] ||= "hello"
+    #     render json: {session: session}
 
-    end
-    # login information for username
-    def create
-        player = Player.find_by!(user_name: params[:username])
-        # byebug
-        if player&.authenticate(params[:password])
-            session[:player_id] = player.id
-            # byebug
-            render json: player, status: :ok
-        else
-            render json: { error: "Invalid username or password" }, status: :unauthorized
-        end
-    end
+    # end
+    # # login information for username
+    # def create
+    #     player = Player.find_by!(user_name: params[:username])
+    #     # byebug
+    #     if player&.authenticate(params[:password])
+    #         session[:player_id] = player.id
+    #         # byebug
+    #         render json: player, status: :ok
+    #     else
+    #         render json: { error: "Invalid username or password" }, status: :unauthorized
+    #     end
+    # end
 
 
-    # logout user
-    def destroy
-        session.delete :user_id
-        head :no_content
-    end
+    # # logout user
+    # def destroy
+    #     session.delete :user_id
+    #     head :no_content
+    # end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
     belongs_to :game_table
     belongs_to :leader_board
-   has_secure_password
+    # has_secure_password
 
     validates :user_name, presence:true, uniqueness: true
  
